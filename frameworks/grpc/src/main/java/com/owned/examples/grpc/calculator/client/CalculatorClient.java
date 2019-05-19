5d49cee29d1b6a702b6112d13489bcdd1223d2c5
javiergarciacotado <jgarciacotado@gmail.com>
2019-05-19 02:20:58 +0200

    Adds hands-on grpc error implementation

--- frameworks/grpc/src/main/java/com/owned/examples/grpc/calculator/client/CalculatorClient.java
+++ frameworks/grpc/src/main/java/com/owned/examples/grpc/calculator/client/CalculatorClient.java
@@ -2,10 +2,13 @@ package com.owned.examples.grpc.calculator.client;
 
 import com.proto.calculator.CalculatorServiceGrpc;
 import com.proto.calculator.CalculatorServiceGrpc.CalculatorServiceImplBase;
+import com.proto.calculator.SquareRootRequest;
+import com.proto.calculator.SquareRootResponse;
 import com.proto.calculator.SumRequest;
 import com.proto.calculator.SumResponse;
 import io.grpc.ManagedChannel;
 import io.grpc.ManagedChannelBuilder;
+import io.grpc.StatusRuntimeException;
 
 public class CalculatorClient extends CalculatorServiceImplBase {
 
@@ -15,6 +18,12 @@ public class CalculatorClient extends CalculatorServiceImplBase {
                 .usePlaintext()
                 .build();
 
+//        unary(channel);
+
+        error(channel);
+    }
+
+    private static void unary(ManagedChannel channel) {
         CalculatorServiceGrpc.CalculatorServiceBlockingStub calculatorServiceStub = CalculatorServiceGrpc.newBlockingStub(channel);
 
         SumRequest sumRequest = SumRequest.newBuilder().setFirstNumber(1).setSecondNumber(2).build();
@@ -23,5 +32,16 @@ public class CalculatorClient extends CalculatorServiceImplBase {
         System.out.println(sumResponse.getResult());
 
         channel.shutdown();
+    }
+
+    private static void error(ManagedChannel channel) {
+
+        CalculatorServiceGrpc.CalculatorServiceBlockingStub calculatorServiceBlockingStub = CalculatorServiceGrpc.newBlockingStub(channel);
+        try {
+            SquareRootResponse squareRootResponse = calculatorServiceBlockingStub.squareRoot(SquareRootRequest.newBuilder().setNumber(-1).build());
+        } catch (StatusRuntimeException statusRuntimeException) {
+            System.out.println("Got an exception for square root!");
+            statusRuntimeException.printStackTrace();
+        }
     }
 }
--- frameworks/grpc/src/main/java/com/owned/examples/grpc/calculator/server/CalculatorServiceImpl.java
+++ frameworks/grpc/src/main/java/com/owned/examples/grpc/calculator/server/CalculatorServiceImpl.java
@@ -1,8 +1,11 @@
 package com.owned.examples.grpc.calculator.server;
 
 import com.proto.calculator.CalculatorServiceGrpc.CalculatorServiceImplBase;
+import com.proto.calculator.SquareRootRequest;
+import com.proto.calculator.SquareRootResponse;
 import com.proto.calculator.SumRequest;
 import com.proto.calculator.SumResponse;
+import io.grpc.Status;
 import io.grpc.stub.StreamObserver;
 
 public class CalculatorServiceImpl extends CalculatorServiceImplBase {
@@ -18,5 +21,15 @@ public class CalculatorServiceImpl extends CalculatorServiceImplBase {
 
         responseObserver.onNext(sumResponse);
         responseObserver.onCompleted();
+    }
+
+    @Override
+    public void squareRoot(SquareRootRequest request, StreamObserver<SquareRootResponse> responseObserver) {
+        int number = request.getNumber();
+        if (number >= 0) {
+            responseObserver.onNext(SquareRootResponse.newBuilder().setNumber(Math.sqrt(number)).build());
+        } else {
+            responseObserver.onError(Status.INVALID_ARGUMENT.withDescription("The number being sent is not positive").augmentDescription("Number: " + number).asRuntimeException());
+        }
     }
 }
